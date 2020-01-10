b1080eaed952e8cfff07fa705ca4b4fe2f4371c9
zwimer <zwimer@gmail.com>
2020-01-10 13:36:53 +0000

    typo

--- core-modules/start.py
+++ core-modules/start.py
@@ -2,6 +2,6 @@ from utils import reply
 
 
 def invoke(updater, *_):
-    msg = 'Welcome to zac-bot.'
+    msg = 'Welcome to zac-bot. '
     msg += 'Invoke /help to see what commands are availabe.'
     reply(updater, msg)
